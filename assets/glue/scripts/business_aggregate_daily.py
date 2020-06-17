2e5a3bb15a7b0f5bafc04bd719405bd56f671bf4
Sascha Rodekamp <sascha.rodekamp@gmail.com>
2020-06-17 15:01:28 +0200

    add daily aggregation based on intermediate date indicators

--- assets/glue/scripts/business_aggregate_daily.py
+++ assets/glue/scripts/business_aggregate_daily.py
@@ -1,4 +1,6 @@
 import sys
+import boto3
+import datetime
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
@@ -8,7 +10,7 @@ from awsglue.dynamicframe import DynamicFrame
 from pyspark.sql.functions import *
 
 ## @params: [JOB_NAME]
-args = getResolvedOptions(sys.argv, ['JOB_NAME', 'db_name', 'business_zone_bucket'])
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'db_name', 'business_zone_bucket', 'temp_workflow_bucket'])
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -16,21 +18,24 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-cleanedMeterDataSource = glueContext.create_dynamic_frame.from_catalog(database = args['db_name'], table_name = "daily", transformation_ctx = "cleanedMeterDataSource")
-
-dailyAggregatedIntervalReads = cleanedMeterDataSource.toDF() \
-    .filter("reading_type = 'INT'") \
-    .groupby('meter_id', 'date_str') \
-    .agg(sum("reading_value").alias("Sum Reading Value")) 
-
-dfAggregatedReads = DynamicFrame.fromDF(dailyAggregatedIntervalReads, glueContext, "dfAggregatedReads")
+# read date information to know which data should be aggregated or re-aggregated
+tempDataFile = s3.Object('glue-workflow-temp', 'glue_workflow_distinct_dates')
+dateList = tempDataFile.get()['Body'].read().decode().split(",")
 
 business_zone_bucket_path_daily = "s3://" + args['business_zone_bucket'] + "/aggregated/daily"
 
-glueContext.write_dynamic_frame.from_options(frame = dfAggregatedReads, \
-    connection_type = "s3", \
-    connection_options = {"path": business_zone_bucket_path_daily, "partitionKeys": ["date_str"]},\
-    format = "parquet", \
-    transformation_ctx = "businessZone")
+for dateStr in dateList:
+    cleanedMeterDataSource = glueContext.create_dynamic_frame.from_catalog(database = "meter-data", table_name = "daily", transformation_ctx = "cleanedMeterDataSource", push_down_predicate = "(reading_type == 'INT' and date_str == '{}')".format(dateStr))
+
+    dailyAggregatedIntervalReads = cleanedMeterDataSource.toDF() \
+        .groupby('meter_id', 'date_str') \
+        .agg(sum("reading_value").alias("SumReadingValue"))
+
+    dailyAggregatedIntervalReads\
+      .write\
+      .mode("overwrite")\
+      .format("parquet")\
+      .partitionBy("date_str")\
+      .save(business_zone_bucket_path_daily)
 
 job.commit()
