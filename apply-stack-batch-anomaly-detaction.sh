9003ff75ce47e0b5252b863c0f0ebbfb363adf7b
Sascha Rodekamp <sascha.rodekamp@gmail.com>
2020-07-31 13:37:48 +0200

    fix fbprophet layer

--- apply-stack-batch-anomaly-detaction.sh
+++ apply-stack-batch-anomaly-detaction.sh
@@ -2,4 +2,4 @@
 aws cloudformation create-stack --stack-name batch-anomaly-detection \
                                 --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
                                 --template-body file://templates/prediction/batch-anomaly-detection.yaml \
-                                --parameters file://stack-parameter2.json
+                                --parameters file://stack-parameter-batch-anomaly-detaction.json
