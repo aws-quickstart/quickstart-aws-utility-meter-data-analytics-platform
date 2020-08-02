591d2128b90ded827d93a29bb7a5bf988d4cfb09
Sascha Rodekamp <sascha.rodekamp@gmail.com>
2020-08-02 14:36:30 +0200

    merge ml and prediction function CF templates

--- apply-stack-batch-anomaly-detaction.sh
+++ apply-stack-batch-anomaly-detaction.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-aws cloudformation create-stack --stack-name batch-anomaly-detection \
+aws cloudformation create-stack --stack-name ml-prediction-pipeline \
                                 --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
-                                --template-body file://templates/prediction/batch-anomaly-detection.yaml \
-                                --parameters file://stack-parameter-batch-anomaly-detaction.json
+                                --template-body file://templates/prediction/template.yaml \
+                                --parameters file://stack-parameter-ml-prediction-pipeline.json
