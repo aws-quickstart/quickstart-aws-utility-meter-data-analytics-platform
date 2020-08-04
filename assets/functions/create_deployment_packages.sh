5f6c77b76be40c22c11ca34325950fb5674c8d46
Sascha Rodekamp <sascha.rodekamp@gmail.com>
2020-08-04 15:31:36 +0200

    integrate anomaly detection into ML pipeline

--- assets/functions/create_deployment_packages.sh
+++ assets/functions/create_deployment_packages.sh
@@ -8,9 +8,13 @@ functions=("upload_result"  \
            "prepare_batch"  \
            "meter_forecast" \
            "get_anomaly"  \
-           "batch_anomaly_detection")
+           "batch_anomaly_detection" \
+           "crawler/trigger_glue_crawler" \
+           "crawler/get_glue_crawler_state")
 
 for lambda_folder in ${functions[*]};
 do
-   zip -9jqr "$PWD/packages/${lambda_folder}.zip" "./${lambda_folder}"
+   function_name=${lambda_folder////_}
+   echo $function_name
+   zip -9jqr "$PWD/packages/${function_name}.zip" "./${lambda_folder}"
 done
