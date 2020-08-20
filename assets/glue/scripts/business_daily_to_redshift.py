00c15268608c9d719a4dffb237a193ff09f02931
Sascha Rodekamp <sascha.rodekamp@gmail.com>
2020-08-20 15:23:45 +0200

    create table in redshift before storing data

--- assets/glue/scripts/business_daily_to_redshift.py
+++ assets/glue/scripts/business_daily_to_redshift.py
@@ -8,6 +8,21 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
+create_table_sql = "create table if not exists daily("\
+                        "meter_id	        VARCHAR(MAX)    ,   "\
+                        "reading_value	    FLOAT8          ,   "\
+                        "obis_code	        VARCHAR(32)     , "\
+                        "week_of_year	    INT4            , "\
+                        "day_of_month	    INT4            , "\
+                        "month	            INT4            , "\
+                        "year	            INT4            , "\
+                        "hour	            INT4            , "\
+                        "minute	            INT4            , "\
+                        "reading_date_time	timestamp       , "\
+                        "reading_type	    VARCHAR(32)     , "\
+                        "date_str	        VARCHAR(16)      )"\
+                        "DISTKEY(meter_id) SORTKEY(meter_id, year);"
+
 ## @params: [TempDir, JOB_NAME]
 args = getResolvedOptions(sys.argv, ['TempDir','JOB_NAME', 'db_name', 'redshift_connection', 'temp_workflow_bucket'])
 
@@ -18,7 +33,7 @@ job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
 allDaily = glueContext.create_dynamic_frame.from_catalog(database = args['db_name'], table_name = "daily", transformation_ctx = "allDaily")
-glueContext.write_dynamic_frame.from_jdbc_conf(frame = allDaily, catalog_connection = args['redshift_connection'], connection_options = {"dbtable": "daily", "database": args['db_name']}, redshift_tmp_dir = args["TempDir"], transformation_ctx = "allDaily")
+glueContext.write_dynamic_frame.from_jdbc_conf(frame = allDaily, catalog_connection = args['redshift_connection'], connection_options = {"preactions":create_table_sql, "dbtable": "daily", "database": args['db_name']}, redshift_tmp_dir = args["TempDir"], transformation_ctx = "allDaily")
 
 # allAggDaily = glueContext.create_dynamic_frame.from_catalog(database = args['db_name'], table_name = "aggregated_daily", transformation_ctx = "allDaily")
 # glueContext.write_dynamic_frame.from_jdbc_conf(frame = allAggDaily, catalog_connection = args['redshift_connection'], connection_options = {"dbtable": "aggregated_daily", "database": args['db_name']}, redshift_tmp_dir = args["TempDir"], transformation_ctx = "allAggDaily")
