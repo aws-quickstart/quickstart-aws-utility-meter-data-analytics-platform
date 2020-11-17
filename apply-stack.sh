1eaf90562955b7099230735a51d43c1cdd0a17d6
Sascha Rodekamp <sascha.rodekamp@gmail.com>
2020-11-17 13:51:16 +0100

    rebuild lambda packagE

--- apply-stack.sh
+++ apply-stack.sh
@@ -2,4 +2,5 @@
 aws cloudformation create-stack --stack-name meter-data-lake \
                                 --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM \
                                 --template-body file://templates/workload.template.yaml \
-                                --parameters file://stack-parameter.json
+                                --parameters file://stack-parameter.json \
+                                --region us-east-1
