7ddb180ef64023c5b5c936197da91dba85f80e2f
Sascha Rodekamp <sascha.rodekamp@gmail.com>
2020-11-22 13:16:09 +0100

    add region flag

--- delete-all-buckets.sh
+++ delete-all-buckets.sh
@@ -22,6 +22,6 @@ do
 done
 
 echo "Deleting stack meter-data-lake"
-aws cloudformation delete-stack --stack-name meter-data-lake
+aws cloudformation delete-stack --stack-name meter-data-lake --region us-east-1
 
 aws cloudformation wait stack-delete-complete --stack-name meter-data-lake
